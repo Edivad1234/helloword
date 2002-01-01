5e498a0298243bb70442a86270516e67e6ef6032
alunno <alunno@debian13>
2002-01-01 19:11:11 +0100

    Fatto

--- eqprimogrado.py
+++ eqprimogrado.py
@@ -11,30 +11,12 @@ b = input ("b=")
 if a==0:
 #Se il valore della x e' 0 l'equazione e' impossibile    
      print "L'equazione e' impossibile"
-else:
 
-        if a % b==0:
-             x = a/-b
-             print ("X="),x 
-        
-        else: 
-          if (a%2==0) and (b%2==0):
-             a = a/2
-             b = b/2 
-             print ("X="),-a
-             print ("   "),b
-           
-          
-          elif (a%3==0) and (b%3==0):
-                a = a/3
-                b = b/3
-                print ("X="),-a
-                print ("   "),b
-             
-          
-          elif (a%5==0) and (b%5==0):
-                      a = a/5
-                      b = b/5
-                      print ("X="),-a
-                      print ("   "),b
-               
+elif a==0 and b==0:
+    print "L'equazione e' sempre verificata"
+
+elif b==0:
+    print "x = 0"
+else :
+    soluzione = -float (b)/float (a)
+    print "La soluzione e'", soluzione 
