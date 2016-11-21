0516ea9585218ef483b38d441b0616607478effc
Davide A <davide.antonucci5@gmail.com>
2016-11-21 22:15:09 +0100

    Superficie e volume sfera

---
+++ sfera.py
@@ -0,0 +1,4 @@
+pi = 3.1415
+raggio = input("Inserire il raggio della sfera: ")
+print ("La superficie della sfera e': "), pi*4*(raggio*raggio)
+print ("Il volume della sfera e': "), (pi*4*(raggio*raggio*raggio))/3
