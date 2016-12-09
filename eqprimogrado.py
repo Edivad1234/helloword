42ea9ec4f54a52bbe1e203db00c4e8d7106c2f02
ALUNNO <alunno@debian15>
2016-12-09 11:27:42 +0100

    Fatto

--- eqprimogrado.py
+++ eqprimogrado.py
@@ -3,38 +3,17 @@
 import math
 
 a = input ("ax=")
-#Valore della x
+#Vadella x
 
 b = input ("b=")
-#Termine noto
+#Terminenoto
 
 if a==0:
-#Se il valore della x e' 0 l'equazione e' impossibile    
-     print "L'equazione e' impossibile"
-else:
+    print "L'eqauzione e' impossibile"
 
-        if a % b==0:
-             x = a/-b
-             print ("X="),x 
-        
-        else: 
-          if (a%2==0) and (b%2==0):
-             a = a/2
-             b = b/2 
-             print ("X="),-a
-             print ("   "),b
-           
-          
-          elif (a%3==0) and (b%3==0):
-                a = a/3
-                b = b/3
-                print ("X="),-a
-                print ("   "),b
-             
-          
-          elif (a%5==0) and (b%5==0):
-                      a = a/5
-                      b = b/5
-                      print ("X="),-a
-                      print ("   "),b
-               
+elif a==0 and b==0:
+    print "L'equazione e' sempre verificata"
+
+else :
+   soluzione= float (-b)/float (a)
+   print "x= ",soluzione 
