5f65b71e989cbdaafa7e6dfeb4c10bb5c1bd7af6
ALUNNO <alunno@debian15>
2016-12-09 11:59:10 +0100

    Fatto

---
+++ eqsecondogrado.py
@@ -0,0 +1,31 @@
+import math
+
+a = input ("ax^2=")
+#Coefficente della x^2
+b = input ("bx=")
+#Coefficente della x
+c = input ("c=")
+#Termine noto
+
+if a==0 and b==0 and c==0:
+    print "Non e' valida."
+
+elif a==0 and b==0:
+    print c ,"=0"
+
+elif a==0:
+    r= float(c)/float(b)*(-1.0)
+    print "x=",r
+
+else:
+    delta = pow(b, 2) - 4.0*(a*c)
+
+if delta<0 :
+       print "L'equazione con delta minore di 0, e' impossibile"
+
+else:    
+    delta= pow(b,2) - 4.0*(a*c)
+    soluz=(((b*(-1))+math.sqrt(delta)))/(2*a)
+    soluz2=(((b*(-1))-mathsqrt(delta)))/(2*a)
+
+
